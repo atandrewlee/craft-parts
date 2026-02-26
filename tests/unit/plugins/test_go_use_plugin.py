2ad1beb0f7ed40af32fc1bd7458979f6054cd301
Andrew Lee <andrew.lee@canonical.com>
2026-02-26 12:37:27 -0500

    didn't impore re into test; fixed it

--- tests/unit/plugins/test_go_use_plugin.py
+++ tests/unit/plugins/test_go_use_plugin.py
@@ -15,6 +15,8 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
+import re
+
 import pytest
 from craft_parts import errors
 from craft_parts.infos import PartInfo, ProjectInfo
